171f86fd4baf872b3c4e89ca513f456d00e3dc4c
mathieue <emile.mathieu@eleves.enpc.fr>
2015-05-23 00:14:45 +0200

    try to implement EM algorithm for paper 1

--- code/code.py
+++ code/code.py
@@ -56,37 +56,69 @@ betaNew = 1
 weight = np.zeros([X.shape[1],nbOfExperts])
 gamma = np.zeros(nbOfExperts)
 
-def eta(w,g,t,i):
-    print w.shape
-    print g.shape
-    print w[:,t].shape
-    print X[i,:].shape
-    a = -np.inner(w[:,t],X[i,:])
-    return expit(a-g[t])
+def eta(U,V,t,i):
+    return expit(np.inner(U[:,t],X[i,:])+V[t])
     
-def f(x):
-    #eta(t,i)*p[i,1]+(1-eta(t,i))**p[i,0]
+def fopt(x):
+    a = x[0:alpha.shape[0]]
+    #print "a.shape", a.shape
+    b = x[alpha.shape[0]:alpha.shape[0]+1][0]
+    #print "b",b
+    g = x[alpha.shape[0]+1:alpha.shape[0]+1+gamma.shape[0]]
+    #print "g.shape", g.shape
+    w = np.reshape(x[alpha.shape[0]+1+gamma.shape[0]:alpha.shape[0]+1+gamma.shape[0]+np.prod(weight.shape)],(weight.shape[0],weight.shape[1]))
+    #print "w.shape", w.shape    
+    res = 0
+    for i in range(X.shape[0]):
+        k = sum([p[i,1]*((1-Y[i,t])*np.log(1-eta(w,g,t,i))+Y[i,t]*np.log(eta(w,g,t,i)))+p[i,0]*(Y[i,t]*np.log(1-eta(w,g,t,i))+(1-Y[i,t])*np.log(eta(w,g,t,i))) for t in range(nbOfExperts)])
+        l = p[i,1]*np.log(expit(np.inner(a,X[i,:])+b)) + p[i,0]*np.log(1-expit(np.inner(a,X[i,:])+b))
+        res = res + k + l
+    return res
+    
+def gradfopt(x):
+    a = x[0:alpha.shape[0]]
+    b = x[alpha.shape[0]:alpha.shape[0]+1][0]
+    g = x[alpha.shape[0]+1:alpha.shape[0]+1+gamma.shape[0]]
+    w = np.reshape(x[alpha.shape[0]+1+gamma.shape[0]:alpha.shape[0]+1+gamma.shape[0]+np.prod(weight.shape)],(weight.shape[0],weight.shape[1]))
+    
+    gradalpha = np.zeros(alpha.shape[0])  
+    gradbeta = 0
+    gradgamma = 0
+    gradweight = np.zeros(weight.shape[0])  
+    
     for i in range(X.shape[0]):
         pass
-        #np.log(sum([p[i,0]+eta(t,i)*(p[i,1]-p[i,0]) for t in range(nbOfExperts)]))
-        #np.log()
+        gradalpha = gradalpha + (p[i,1]-expit(np.inner(a,X[i,:])+b))*X[i,:]
+        gradbeta = gradbeta + p[i,1]-expit(np.inner(a,X[i,:])+b)
+        gradgamma = gradgamma + p[i,1]*(-eta(w,g,t,i)+Y[i,t]) + p[i,0]*(1-eta(w,g,t,i)-Y[i,t])
+        gradweight = gradweight + (p[i,1]*(-eta(w,g,t,i)+Y[i,t]) + p[i,0]*(1-eta(w,g,t,i)-Y[i,t]))*X[i,:]
+    return np.hstack((gradalpha,gradbeta,gradgamma,gradweight))
     
 iter = 0
-while np.inner(alpha-alphaNew,alpha-alphaNew)+(beta-betaNew)**2>espilon:
+while (np.inner(alpha-alphaNew,alpha-alphaNew)+(beta-betaNew)**2>espilon and iter<3):
     print "iteration:",iter
-    alpha = alphaNew
-    beta = betaNew
     ###E-STEP
-        #((1-eta(t,i))**abs(Y[i,t]-Z[i]))*(eta(t,i)**(1-abs(Y[i,t]-Z[i])))
+    print "E-STEP"
     for i,x in enumerate(X):
         #P0 = ((1-eta(t,i))**abs(Y[i,t]-0))*(eta(t,i)**(1-abs(Y[i,t]-0)))
         #P1 = ((1-eta(t,i))**abs(Y[i,t]-1))*(eta(t,i)**(1-abs(Y[i,t]-1)))
         P2 = expit(-np.inner(alpha,X[i,:])-beta)
-        p[i,1]=np.prod([((1-eta(weight,gamma,t,i))**abs(Y[i,t]-1))*(eta(gamma,beta,t,i)**(1-abs(Y[i,t]-1)))*P2 for t in range(nbOfExperts)])
-        p[i,0]=np.prod([((1-eta(weight,gamma,t,i))**abs(Y[i,t]-0))*(eta(gamma,beta,t,i)**(1-abs(Y[i,t]-0)))*(1-P2) for t in range(nbOfExperts)])
-        
+        p[i,1]=np.prod([((1-eta(weight,gamma,t,i))**abs(Y[i,t]-1))*(eta(weight,gamma,t,i)**(1-abs(Y[i,t]-1)))*P2 for t in range(nbOfExperts)])
+        p[i,0]=np.prod([((1-eta(weight,gamma,t,i))**abs(Y[i,t]-0))*(eta(weight,gamma,t,i)**(1-abs(Y[i,t]-0)))*(1-P2) for t in range(nbOfExperts)])
+        #p[i,0]=1-p[i,1]
+        r = p[i,1]+p[i,0]
+        p[i,1]=p[i,1]/r
+        p[i,0]=p[i,0]/r
     
     ###M-STEP
-    #optimize.fmin_bfgs(f=,x0=,fprime=)
-    
+    print "M-STEP"
+    argmax = optimize.fmin_bfgs(f=fopt,x0=np.ones(alpha.shape[0]+1+gamma.shape[0]+np.prod(weight.shape)),fprime=gradfopt)
+    alpha = argmax[0:alpha.shape[0]]
+    beta = argmax[alpha.shape[0]:alpha.shape[0]+1][0]
+    gamma = argmax[alpha.shape[0]+1:alpha.shape[0]+1+gamma.shape[0]]
+    weight = np.reshape(argmax[alpha.shape[0]+1+gamma.shape[0]:alpha.shape[0]+1+gamma.shape[0]+np.prod(weight.shape)],(weight.shape[0],weight.shape[1]))
     iter = iter + 1
+    
+test = np.hstack((alpha,beta,gamma,np.reshape(weight,np.prod(weight.shape))))
+print f(test)
+print gradf(test)
