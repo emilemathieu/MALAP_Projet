97545faf843fcf21c70453871cefe602dc41967e
pesneaut <thomas.pesneau@eleves.enpc.fr>
2015-06-04 17:51:31 +0200

    optimisation de la fonction

--- code/learning_from_crowds.py
+++ code/learning_from_crowds.py
@@ -6,6 +6,7 @@ Created 01/06/2015
 @author: PesneauReizine
 """
 
+import tools
 import numpy as np # module pour les outils mathématiques
 import matplotlib.pyplot as plt # module pour les outils graphiques
 from sklearn.cluster import KMeans #module pour trouver des clusters par la méthode k-means
@@ -17,6 +18,10 @@ import operator
 from random import random
 
 ###Every array is in column
+##Y is a (350,5) matrix
+##X is a (350,34) matrix
+##Z is a (350,1) vector
+##mu is a (350,1) vector
 
 nbOfExperts = 5
 
@@ -56,8 +61,11 @@ for i in range(Y.shape[0]):
 
 
 def p(w,x):#x a nbOfExperts colonnes et 34 lignes
-    z = w*(X.transpose())
-    p = 1/(1+exp(-z))
+    z = np.inner(X,w)
+    p = np.ones(z.shape[0])
+    for i in range(z.shape[0]):
+        p[i] = 1/(1+exp(-z[i]))
+    print " taille p : ",p.shape
     return p
 
 def a(alpha,Y):
@@ -74,22 +82,52 @@ def a(alpha,Y):
 def b(beta,Y):
     return a(1-beta,Y)
 
+    
+def gradp(x,mu,w):
+    return np.inner(tools.to_line(mu-p(w,x)),x.transpose())
+
+def hessianp(x,w):
+    result=0
+    sigma=p(w,x)
+    for i in range(x.shape[1]):
+        xinte=x[i,:]
+        sigmatemp=sigma[i]
+        result+=sigmatemp*(1-sigmatemp)*np.dot(tools.to_col(xinte),tools.to_line(xinte))
+    return result
+
+
+
 ###EM Algorithm
-mu = (1/nbOfExperts)*sum(Y.transpose())
-w = (1./5)*np.ones(5)
-#alphanew = np.inner(mu.transpose(),Y)/sum(mu)
-#betanew = (1-mu)*(1-Y)/sum(1-mu)
+mu = (1./nbOfExperts)*sum(Y.transpose())
+w = (1./34)*np.ones(34)
+alphanew = np.inner(tools.to_line(mu),Y.transpose())/sum(mu)
+betanew = np.inner(tools.to_line(1-mu),1-Y.transpose())/sum(1-mu)
 
 
+grad = gradp(X,mu,w)
+hess=hessianp(X,w)
+def fopt(w):
+    return p(w,X)
+def gradopt(w):
+    return gradp(X,mu,w)
+def hessopt(w):
+    return hessianp(x,w)
+#optimize.newton(func=fopt,x0=w,fprime=gradopt,fprime2=hessopt)
 
 ###Finir l'algorithme EM
 ##for k in range(1000):
 ##    ###E-step
+##    print "E-step"
 ##    a = a(alphanew,Y)
 ##    b = b(betanew,Y)
 ##    p = p(w,x)
-    
- 
+##    ##Calculer mu
+##
+##    ###M-step
+##    alphanew = np.inner(tools.to_line(mu),Y.transpose())/sum(mu)
+##    betanew = np.inner(tools.to_line(1-mu),1-Y.transpose())/sum(1-mu)
+##    ##Calculer w
+
     
 
 
