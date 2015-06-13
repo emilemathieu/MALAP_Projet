8bd5864f81090ee8d2465dfb9c66d43b8079db61
mathieue <emile.mathieu@eleves.enpc.fr>
2015-06-13 12:38:18 +0200

    ROC curves added

--- code/code.py
+++ code/code.py
@@ -13,57 +13,15 @@ from scipy.optimize import minimize #module pour optimiser des fonction
 from scipy.special import expit
 from sklearn import cross_validation as cv
 from collections import Counter
-
+from sklearn.metrics import roc_curve, auc
+from scipy import interp
 
 #cd ~/Desktop/IMI/MALAP/Projet/code/
 
 #########Modeling annotator expertise:
 #########Learning when everybody knows a bit of something
-
-#################   Data Loading and Formating    ################# 
-
-nbOfExperts = 5
-
-### READING DATA
-def load_ionosphere(filename):
-    with open(filename,"r") as f:
-                 f.readline()
-                 data =[ [x for x in l.split(',')] for l in f]
-    tmp = np.array(data)
-    tmp[:,34][np.where(tmp[:,34]=='b\n')]=0
-    tmp[:,34][np.where(tmp[:,34]=='g\n')]=1
-    print tmp.shape
-    np.random.shuffle(tmp)
-    return tmp[:,0:34].astype(float),tmp[:,34].astype(int)
-    
-X,Z = load_ionosphere("ionosphere.txt")
-
-###CLUSTERING DATA WITH KMEANS
-kmeans = KMeans(nbOfExperts)
-clusters = kmeans.fit_predict(X)
-print kmeans.inertia_
-
-###CREATING EXPERTS LABELS
-Y=np.zeros([X.shape[0],nbOfExperts])
-Y[:,clusters]=Z
-for i,cluster in enumerate(clusters):
-    Y[i,cluster]=Z[i]
-    for j in range(Y.shape[1]):
-        if j!=i and np.random.random_sample()<=0.35:
-            Y[i,j]=1-Z[i]
             
 #################    Learning parameters    ################# 
-            
-### Initialization            
-alpha = np.ones(X.shape[1])
-beta = 1
-espilon = 1e-5
-p = np.zeros([Z.shape[0],2])
-
-alphaNew = np.zeros(X.shape[1])
-betaNew = 0
-weight = np.zeros([X.shape[1],nbOfExperts])
-gamma = np.zeros(nbOfExperts)
 
 def eta(U,V,t,i):
     return expit(np.inner(U[:,t],X[i,:])+V[t])
@@ -118,10 +76,51 @@ def gradfopt(x):
         gradweight = gradweight + np.reshape(np.array([(-1)**Y[i,t]*(p[i,0]-p[i,1])*expit(np.inner(w[:,t],Xi)+g[t])*(1-expit(np.inner(w[:,t],Xi)+g[t]))*Xi  for t in range(nbOfExperts)]),np.prod(weight.shape))
     return -np.hstack((gradalpha,gradbeta,gradgamma,gradweight))
 
+#################   Data Loading and Formating    ################# 
+
+nbOfExperts = 5
+
+### READING DATA
+def load_ionosphere(filename):
+    with open(filename,"r") as f:
+                 f.readline()
+                 data =[ [x for x in l.split(',')] for l in f]
+    tmp = np.array(data)
+    tmp[:,34][np.where(tmp[:,34]=='b\n')]=0
+    tmp[:,34][np.where(tmp[:,34]=='g\n')]=1
+    #print tmp.shape
+    np.random.shuffle(tmp)
+    return tmp[:,0:34].astype(float),tmp[:,34].astype(int)
+    
+X,Z = load_ionosphere("ionosphere.csv")
+
+###CLUSTERING DATA WITH KMEANS
+kmeans = KMeans(nbOfExperts)
+clusters = kmeans.fit_predict(X)
+print kmeans.inertia_
+
+###CREATING EXPERTS LABELS
+Y=np.zeros([X.shape[0],nbOfExperts])
+for i,cluster in enumerate(clusters):
+    Y[i,:]=Z[i]
+    for j in range(Y.shape[1]):
+        if j!=cluster and np.random.random_sample()<0.35:
+            Y[i,j]=1-Z[i]
+
+### Initialization            
+alpha = np.ones(X.shape[1])
+beta = 1
+epsilon = 1e-8
+p = np.zeros([Z.shape[0],2])
+
+alphaNew = np.zeros(X.shape[1])
+betaNew = 0
+weight = np.zeros([X.shape[1],nbOfExperts])
+gamma = np.zeros(nbOfExperts)
     
 ####EM-Algorithm
 iter = 0
-while (np.inner(alpha-alphaNew,alpha-alphaNew)/alpha.shape[0]+(beta-betaNew)**2>espilon):
+while (np.inner(alpha-alphaNew,alpha-alphaNew)/alpha.shape[0]+(beta-betaNew)**2>epsilon):
     print "iteration:",iter
     print "convergence criterion:", np.inner(alpha-alphaNew,alpha-alphaNew)+(beta-betaNew)**2
     
@@ -150,7 +149,7 @@ while (np.inner(alpha-alphaNew,alpha-alphaNew)/alpha.shape[0]+(beta-betaNew)**2>
     
     ###M-STEP
     print "M-STEP"
-    r#es = minimize(fopt, theta_old, method='L-BFGS-B', options={'disp': True})
+    #res = minimize(fopt, theta_old, method='L-BFGS-B', options={'disp': True})
     res = minimize(fopt, theta_old, method='L-BFGS-B', options={'disp': True, 'gtol':1})
     #res = minimize(fopt, theta_old, options={'disp': True, 'maxiter': 5})
     #res = minimize(fopt, theta_old, method='BFGS', options={'disp': True, 'maxiter': 10})
@@ -175,34 +174,73 @@ weight = np.reshape(theta_new[alpha.shape[0]+1+gamma.shape[0]:alpha.shape[0]+1+g
 
 
 ### ROC for EM-algorithm
-sensitivity=np.zeros(9)
-antispecificity=np.zeros(9)
-for s in range(1,10):
-    threshold=s*1.0/10
-    print threshold
-    for i,instance in enumerate(X):
-        sensitivity[s-1]=sensitivity[s-1]+int(expit(np.inner(alpha,instance)+beta)>threshold)*int(Z[i]==1)
-        antispecificity[s-1]=antispecificity[s-1]+int(expit(np.inner(alpha,instance)+beta)<threshold)*int(Z[i]==0)
-        #score=score+int(Z[i]==int(expit(np.inner(alphaNew,instance)+betaNew)>threshold))
-    sensitivity[s-1]=sensitivity[s-1]*1.0/sum(Z==1)
-    antispecificity[s-1]=1-(antispecificity[s-1]*1.0/sum(Z==0))
-    print "sensitivity", sensitivity[s-1]
-    print "antispecificity", antispecificity[s-1]
 
-plt.plot(antispecificity,sensitivity)
+fpr, tpr, thresholds = roc_curve(Z, expit(np.sum(np.multiply(X,np.tile(alpha,(X.shape[0],1))),1)+beta))
+fpr, tpr, thresholds = roc_curve(Z, p[:,1])
+
+roc_auc = auc(fpr, tpr)
+
+plt.plot(fpr, tpr, 'k--',label='Bernouilli ROC (area = %0.2f)' % roc_auc, lw=2)
+plt.xlim([-0.05,1.05])
+plt.ylim([-0.05,1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
     
-### Classification for Logistic regression on majority voting
+### Classification for Logistic regression on majority voting & concatenating
 
+def LogisticRegressionROC(Data,Label,legend):
+    Majority_Logistic = LogisticRegression()
+    Majority_Logistic.verbose=0
+    N_folds=5
+    kf= cv.KFold(X.shape[0],n_folds=N_folds)
+
+    mean_tpr = 0.0
+    mean_fpr = np.linspace(0, 1, 100)
+
+    for i, (train, test) in enumerate(kf):
+        probas_ = Majority_Logistic.fit(Data[train], Label[train]).predict_proba(Data[test])
+        # Compute ROC curve and area the curve
+        fpr, tpr, thresholds = roc_curve(Label[test], probas_[:, 1])
+        mean_tpr += interp(mean_fpr, fpr, tpr)
+        mean_tpr[0] = 0.0
+        #roc_auc = auc(fpr, tpr)
+        #plt.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
+
+    #plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6), label='Luck')
+    
+    mean_tpr /= len(kf)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    plt.plot(mean_fpr, mean_tpr, 'k--',color=(np.random.random_sample(), np.random.random_sample(), np.random.random_sample()),label='%s ROC (AUC: %0.2f)' % (legend,mean_auc), lw=2)
+    
+    plt.xlim([-0.05,1.05])
+    plt.ylim([-0.05,1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('R.O.C. Curves for iono data')
+    plt.legend(loc="lower right")
+    #plt.show()
+    
 # Majority label
 Y_maj=np.zeros_like(Z)
 for i,y in enumerate(Y):
     Y_maj[i]=Counter(y).most_common()[0][0]
 
-Majority_Logistic = LogisticRegression(verbose=True)
-kf= cv.KFold(X.shape[0],n_folds=5)
-res_test=[]
-for cvtrain,cvtest in kf:
-    Majority_Logistic.fit(X[cvtrain],Y_maj[cvtrain])
-    res_test+=[Majority_Logistic.score(X[cvtest],Y_maj[cvtest])]
+# concatenate labels
+X_concatenate=np.repeat(X,5,axis=0)
+Y_concatenate=np.reshape(Y,np.prod(Y.shape))
 
-    
+LogisticRegressionROC(X_concatenate,Y_concatenate,"L.R. Concatenation")
+
+LogisticRegressionROC(X,Y_maj,"L.R. Majority")
+
+LogisticRegressionROC(X,Y[:,0],"L.R.-Annotator1")
+LogisticRegressionROC(X,Y[:,1],"L.R.-Annotator2")
+LogisticRegressionROC(X,Y[:,2],"L.R.-Annotator3")
+LogisticRegressionROC(X,Y[:,3],"L.R.-Annotator4")
+LogisticRegressionROC(X,Y[:,4],"L.R.-Annotator5")
+plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6), label='Luck')
+plt.show()
