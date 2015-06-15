6fe55021246f4d7be481a96f2282b1a9897cf2c3
CharlesReizine <creizine@gmail.com>
2015-06-15 10:25:07 +0200

    Courbes ROC terminees

--- code/learning_from_crowds.py
+++ code/learning_from_crowds.py
@@ -16,7 +16,11 @@ from math import exp,log
 from functools import reduce
 import operator
 from random import random
-
+from sklearn import cross_validation as cv
+from collections import Counter
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve, auc
+from scipy import interp
 ###Every array is in column
 ##Y is a (350,5) matrix
 ##X is a (350,34) matrix
@@ -25,7 +29,7 @@ from random import random
 
 nbOfExperts = 5
 iter_max=100
-tolerance=0.0005
+tolerance=0.0001
 ### READING DATA
 def load_ionosphere(filename):
     with open(filename,"r") as f:
@@ -39,13 +43,13 @@ def load_ionosphere(filename):
     return tmp[:,0:34].astype(float),tmp[:,34].astype(int)
 
 ###Building data
-X,Z = load_ionosphere("ionosphere.txt")
-alpha = np.arange(0.75,1,0.05)#true positive rate
-beta = np.arange(0.5,0.75,0.05)#1-false positive rate
+X,Z = load_ionosphere("../DataSets/ionosphere.txt")
+alpha = np.arange(0.4,0.9,0.1)#true positive rate
+beta = np.arange(0.3,0.75,0.1)#1-false positive rate
 ##build noisy labels based on Two-coin model for annotators
 Y = np.ones([Z.shape[0],nbOfExperts])
 for i in range(Y.shape[0]):
-    r = 100*random()
+    
     z = Z[i]
     if(z):
         for j in range(Y.shape[1]):
@@ -174,6 +178,65 @@ print("beta found ", betanew)
 
 
 
+def LogisticRegressionROC(Data,Label,TrueLabel, Color,legend, marker):
+    Majority_Logistic = LogisticRegression()
+    Majority_Logistic.verbose=0
+    N_folds=5
+    kf= cv.KFold(X.shape[0],n_folds=N_folds)
+
+    mean_tpr = 0.0
+    mean_fpr = np.linspace(0, 1, 100)
+
+    for i, (train, test) in enumerate(kf):
+        probas_ = Majority_Logistic.fit(Data[train], Label[train]).predict_proba(Data[test])
+        # Compute ROC curve and area the curve
+        fpr, tpr, thresholds = roc_curve(TrueLabel[test], probas_[:, 1])
+        mean_tpr += interp(mean_fpr, fpr, tpr)
+        mean_tpr[0] = 0.0
+        #roc_auc = auc(fpr, tpr)
+        #plt.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
+    
+    mean_tpr /= len(kf)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    
+    plt.plot(mean_fpr, mean_tpr, marker ,color=Color,label='%s AUC:%0.2f' % (legend,mean_auc), lw=2)
+    
+    plt.xlim([-0.05,1.05])
+    plt.ylim([-0.05,1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('R.O.C. Curves for iono data')
+    plt.legend(loc="lower right")
+    #plt.show()
+    
+# Majority label
+Y_maj=np.zeros_like(Z)
+for i,y in enumerate(Y):
+    Y_maj[i]=Counter(y).most_common()[0][0]
+
+# concatenate labels
+#X_concatenate=np.repeat(X,nbOfExperts,axis=0)
+#Y_concatenate=np.reshape(Y,np.prod(Y.shape))
+
+### ROC for EM-algorithm
+fpr, tpr, thresholds = roc_curve(Z, mu)
+plt.plot(fpr, tpr, '-.',color=(1,0,1), label='Bayesian approach:%0.2f' % auc(fpr, tpr), lw=2)
+
+plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6))
+
+LogisticRegressionROC(np.repeat(X,nbOfExperts,axis=0),np.reshape(Y,np.prod(Y.shape)),np.repeat(Z,nbOfExperts,axis=0),(0,0,1),"L.R. Concatenation",'-.')
+
+LogisticRegressionROC(X,Y_maj,Z,(0,0,0),"L.R. Majority",'-')
+
+for i in range(nbOfExperts):
+    if i%2==0:
+        Color=(1,1,0.31)
+    else:
+        Color=(0.39,1,0.33)
+    LogisticRegressionROC(X,Y[:,i],Z,Color,"L.R.-Annotator{}".format(i+1),'--')
+
+plt.show()
     
 
 
